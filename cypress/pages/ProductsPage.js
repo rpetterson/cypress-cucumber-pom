6269387f56935ecb944e190f2c33823aa3503df2
robpetterson <94540736+robpetterson@users.noreply.github.com>
2023-07-19 16:15:18 +0100

    cart assertion added

--- cypress/pages/ProductsPage.js
+++ cypress/pages/ProductsPage.js
@@ -1,31 +1,31 @@
 class ProductsPage {
-    visit() {
-      cy.visit('/');
-    }
-  buyMostExpensiveProduct(){
-
-    
-      let highestPrice = 0;
-      let highestPricedItem;
-    
-      // Go through each item and find the highest priced one
-      cy.get('.inventory_item').each(($el, index, $list) => {
-        const priceText = $el.find('.inventory_item_price').text();
-        const price = parseFloat(priceText.replace('$', ''));  // Remove the dollar sign and parse the price to a float
-    
-        if (price > highestPrice) {
-          highestPrice = price;
-          highestPricedItem = $el;
-        }
-      }).then(() => {
-        // Once we have found the highest priced item, click on it.
-        cy.wrap(highestPricedItem).find('.btn').click();
-      })
-    }
-    
-
+  visit() {
+    cy.visit('/');
   }
-   
+buyMostExpensiveProduct(){
+
+  
+    let highestPrice = 0;
+    let highestPricedItem;
+  
+    // Go through each item and find the highest priced one
+    cy.get('.inventory_item').each(($el, index, $list) => {
+      const priceText = $el.find('.inventory_item_price').text();
+      const price = parseFloat(priceText.replace('$', ''));  // Remove the dollar sign and parse the price to a float
+  
+      if (price > highestPrice) {
+        highestPrice = price;
+        highestPricedItem = $el;
+      }
+    }).then(() => {
+      // Once we have found the highest priced item, click on it.
+      cy.wrap(highestPricedItem).find('.btn').click();
+    })
+  }
+
+  cartUpdated() {
+    cy.get('#shopping_cart_container > a > span').should('contain', '1');
+  }
   
    
   export const productsPage = new ProductsPage();
