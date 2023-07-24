114453990e3db729df033d635b28928a7fe90db3
robpetterson <94540736+robpetterson@users.noreply.github.com>
2023-07-24 13:10:55 +0100

    para changes

--- cypress.config.js
+++ cypress.config.js
@@ -29,9 +29,7 @@ module.exports = defineConfig({
     env: {
       allureReuseAfterSpec: true,
     },
-  //  projectId: "wd92dz",
-  //  projectId: "9yaff9",
-  //  projectId: "2167gy",
+ 
     projectId: "n21t31",
     
     "viewportWidth": 1920,
